31f2a88b0f94f16c1976f6c8f367e2c382f1564f
mayangzhou <273288461@qq.com>
2021-08-30 23:42:42 +0800

    2021/8/30

---
+++ src/main/java/T14/mainApp.java
@@ -0,0 +1,31 @@
+package T14;
+/*14. 最长公共前缀
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+
+
+示例 1：
+
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+示例 2：
+
+输入：strs = ["dog","racecar","car"]
+输出：""
+解释：输入不存在公共前缀。
+
+
+提示：
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] 仅由小写英文字母组成*/
+public class mainApp {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String[] strings = {"dog","racecar","car"};
+        System.out.println(sol.longestCommonPrefix(strings));
+    }
+}
